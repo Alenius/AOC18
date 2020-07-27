41fbd00642d727d5a3aa585e6fd4baf26f4e575e
Adam Alenius <adam.alenius@gmail.com>
2020-07-27 22:10:55 +0200

    solve 1.1

--- js/day1/index.js
+++ js/day1/index.js
@@ -1,5 +1,22 @@
-import {} from 'ramda'
+const {
+  parseNewlineSeparated,
+  readFile,
+  castStringArrToInt,
+} = require('../utils')
 
-function firstPart() {}
+const { reduce, add } = require('ramda')
 
-console.log('Answer first part: ', firstPart())
+const getInput = async (filename) => {
+  const rawInput = await readFile(filename)
+  const stringInput = parseNewlineSeparated(rawInput)
+  return castStringArrToInt(stringInput)
+}
+
+const firstPart = reduce(add, 0)
+
+async function main() {
+  const input = await getInput('day1/input.txt')
+  console.log({ firstPart: await firstPart(input) })
+}
+
+main()
