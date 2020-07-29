88287d94ca63e9a223181da038bd6c19c658a2f2
Adam Alenius <adam.alenius@gmail.com>
2020-07-29 08:04:48 +0200

    solve 1.2

--- js/day1/index.js
+++ js/day1/index.js
@@ -4,7 +4,7 @@ const {
   castStringArrToInt,
 } = require('../utils')
 
-const { reduce, add } = require('ramda')
+const { reduce, add, length, equals } = require('ramda')
 
 const getInput = async (filename) => {
   const rawInput = await readFile(filename)
@@ -14,9 +14,25 @@ const getInput = async (filename) => {
 
 const firstPart = reduce(add, 0)
 
+const isListOutOfBounds = (list, index) => equals(index, length(list) - 1)
+
+const secondPart = (input) => {
+  let value = input[0]
+  const foundValues = {}
+  let index = 1
+  while (!foundValues[value]) {
+    foundValues[value] = true
+    value = add(value, input[index])
+    index = isListOutOfBounds(input, index) ? 0 : add(1, index)
+  }
+  return value
+}
+
 async function main() {
   const input = await getInput('day1/input.txt')
-  console.log({ firstPart: await firstPart(input) })
+  const firstPartAnswer = await firstPart(input)
+  const secondPartAnswer = secondPart(input)
+  console.log({ firstPartAnswer, secondPartAnswer })
 }
 
 main()
