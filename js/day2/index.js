6a6830d5d0a50a5be8d42bd508fd30aa983ca623
Adam Alenius <adam.alenius@gmail.com>
2020-08-01 21:07:46 +0200

    solve 2.2

--- js/day2/index.js
+++ js/day2/index.js
@@ -1,5 +1,23 @@
 const { parseNewlineSeparated, readFile } = require('../utils')
-const { forEach, uniq, includes } = require('ramda')
+const {
+  forEach,
+  uniq,
+  includes,
+  sort,
+  subtract,
+  map,
+  gt,
+  comparator,
+  lt,
+  findIndex,
+  reduce,
+  indexOf,
+  equals,
+  remove,
+  head,
+  join,
+  split,
+} = require('ramda')
 
 const part1 = (listOfIDs) => {
   let twice = 0
@@ -18,10 +36,47 @@ const part1 = (listOfIDs) => {
   return twice * thrice
 }
 
+const letterComparer = comparator(lt)
+
+const part2 = (listOfIDs) => {
+  const sortedListOfIDs = sort(letterComparer, listOfIDs) // sorts list alphabetically
+  const oneLetterDiffArr = []
+  forEach((idString) => {
+    const currIx = indexOf(idString, sortedListOfIDs)
+    if (currIx < sortedListOfIDs.length - 1) {
+      const nextIdString = sortedListOfIDs[currIx + 1]
+      let letterIx = 0
+      let stringLikeness = reduce(
+        (acc, letterInString) => {
+          const letterLikeness = letterInString
+            .toString()
+            .localeCompare(nextIdString[letterIx])
+          letterIx++
+          return acc + Math.abs(letterLikeness)
+        },
+        0,
+        idString
+      )
+      stringLikeness === 1 && oneLetterDiffArr.push(idString, nextIdString)
+      stringLikeness = 0
+    }
+  }, sortedListOfIDs)
+  const firstWord = oneLetterDiffArr[0]
+  let differingLetterIx
+  let letterIx = 0
+  forEach((it) => {
+    if (it.localeCompare(oneLetterDiffArr[1][letterIx]) !== 0) {
+      differingLetterIx = letterIx
+    }
+    letterIx++
+  }, firstWord)
+  split('', head(oneLetterDiffArr)).splice(differingLetterIx, 1) // this will modify the array
+  return head(oneLetterDiffArr)
+}
 const main = async () => {
   const rawInput = await readFile('day2/input.txt')
   const input = parseNewlineSeparated(rawInput)
-  console.log({ answer1: part1(input) })
+  console.log({ answer1: part1(input), answer2: part2(input) })
 }
 
 main()
