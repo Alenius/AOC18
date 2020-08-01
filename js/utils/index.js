4ebf56b79f1573edd9579a7e7b9aaed466825dfd
Adam Alenius <adam.alenius@gmail.com>
2020-08-02 00:04:44 +0200

    add visual matrix util

--- js/utils/index.js
+++ js/utils/index.js
@@ -1,5 +1,16 @@
 const fs = require('fs')
-const { split, map } = require('ramda')
+const { split, map, forEach, join, append } = require('ramda')
+
+const createBaseMatrix = (height, width) => {
+  const appendDot = append('.')
+  const base = [[], []]
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      base[i] = appendDot(base[i])
+    }
+  }
+  return base
+}
 
 async function readFile(filename) {
   const data = await fs.promises.readFile(filename, 'utf8')
@@ -10,8 +21,14 @@ const parseNewlineSeparated = split('\n')
 
 const castStringArrToInt = map(parseInt)
 
+const printMatrix = (matrix) => {
+  forEach((row) => console.log(join('', row)), matrix)
+}
+
 module.exports = {
   readFile,
   parseNewlineSeparated,
   castStringArrToInt,
+  printMatrix,
+  createBaseMatrix,
 }
